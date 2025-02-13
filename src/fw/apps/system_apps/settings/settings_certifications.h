5b5d49cb495483c955a695a7407fc6bf1ff12f9a
Liam McLoughlin <lmcloughlin@google.com>
2025-02-13 11:40:38 +0000

    fw: drivers: add AS7000 HRM driver code/demo app

--- src/fw/apps/system_apps/settings/settings_certifications.h
+++ src/fw/apps/system_apps/settings/settings_certifications.h
@@ -165,12 +165,11 @@ static const CertificationIds * prv_get_certification_ids(void) {
 #elif defined(BOARD_SPALDING) || defined(BOARD_SPALDING_EVT)
   return &s_certification_ids_spalding;
 #elif PLATFORM_SILK && !defined(IS_BIGBOARD)
-// TODO: remove force-false
-//  if (mfg_info_is_hrm_present()) {
-//    return &s_certification_ids_silk_hr;
-//  } else {
+  if (mfg_info_is_hrm_present()) {
+    return &s_certification_ids_silk_hr;
+  } else {
     return &s_certification_ids_silk;
-//  }
+  }
 #else
   return &s_certification_ids_fallback;
 #endif
